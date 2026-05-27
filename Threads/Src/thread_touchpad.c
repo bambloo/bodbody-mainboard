0ce5973f13baddec3962420259900e9f5ae65f7a
Eric Liu <liugchome2004@163.com>
2026-05-28 01:34:08 +0800

    format code

--- Threads/Src/thread_touchpad.c
+++ Threads/Src/thread_touchpad.c
@@ -1,15 +1,15 @@
 #include "thread_touchpad.h"
-#include "thread_ltdc.h"
+#include "bambloo-i2c.h"
 #include "dma2d.h"
 #include "gpio.h"
 #include "ltdc.h"
 #include "main.h"
 #include "rng.h"
 #include "stdlib.h"
-#include "bambloo-i2c.h"
+#include "thread_ltdc.h"
 #include "tx_api.h"
 
-uint8_t stack[4096];
+uint8_t       stack[4096];
 const uint8_t gt911_address = 0x28;
 
 typedef struct {
@@ -141,142 +141,142 @@ typedef struct {
 } gt911_config_t;
 
 static gt911_config_t gt911_config = {
-    .config_version = 0x63,
-    .x_output_max_low = 0x58,
+    .config_version    = 0x63,
+    .x_output_max_low  = 0x58,
     .x_output_max_high = 0x2,
-    .y_output_max_low = 0x20,
+    .y_output_max_low  = 0x20,
     .y_output_max_high = 0x03,
-    .touch_number = 0x01,
-    .module_switch1 = 0x05,
-    .module_switch2 = 0x00,
-    .shake_count = 0x01,
-    .filter = 0x09,
-    .large_touch = 0x29,
+    .touch_number      = 0x01,
+    .module_switch1    = 0x05,
+    .module_switch2    = 0x00,
+    .shake_count       = 0x01,
+    .filter            = 0x09,
+    .large_touch       = 0x29,
 
-    .noise_reduction = 0x0F,
+    .noise_reduction    = 0x0F,
     .screen_touch_level = 0x55,
     .screen_leave_level = 0x32,
-    .low_power_control = 0x03,
-    .refresh_rate = 0x00,
-    .x_threshold = 0x00,
-    .y_threshold = 0x00,
-    .x_speed_limit = 0x00,
-    .y_speed_limit = 0x00,
-    .space_top_bottom = 0x00,
-    .space_left_right = 0x00,
+    .low_power_control  = 0x03,
+    .refresh_rate       = 0x00,
+    .x_threshold        = 0x00,
+    .y_threshold        = 0x00,
+    .x_speed_limit      = 0x00,
+    .y_speed_limit      = 0x00,
+    .space_top_bottom   = 0x00,
+    .space_left_right   = 0x00,
 
-    .mini_filter = 0x00,
-    .stretch_r0 = 0x00,
-    .stretch_r1 = 0x00,
-    .stretch_r2 = 0x00,
-    .stretch_rm = 0x00,
-    .drv_groupa_num = 0x8C,
-    .drv_groupb_num = 0x2E,
-    .sensor_num = 0x0E,
-    .freqa_factor = 0x29,
-    .freqb_factor = 0x27,
+    .mini_filter     = 0x00,
+    .stretch_r0      = 0x00,
+    .stretch_r1      = 0x00,
+    .stretch_r2      = 0x00,
+    .stretch_rm      = 0x00,
+    .drv_groupa_num  = 0x8C,
+    .drv_groupb_num  = 0x2E,
+    .sensor_num      = 0x0E,
+    .freqa_factor    = 0x29,
+    .freqb_factor    = 0x27,
     .pannel_bitfreql = 0x00,
 
-    .pannel_bitfreqh = 0x3D,
-    .pannel_sensor_timel = 0x00,
-    .pannel_sensor_timeh = 0x00,
-    .pannel_tx_gain = 0x00,
-    .pannel_rx_gain = 0x92,
-    .pannel_dump_shift = 0x03,
-    .drv_frame_controle = 0x1C,
-    .channel_level_up = 0x00,
-    .module_switch3 = 0x00,
-    .gesture_dis = 0x00,
+    .pannel_bitfreqh         = 0x3D,
+    .pannel_sensor_timel     = 0x00,
+    .pannel_sensor_timeh     = 0x00,
+    .pannel_tx_gain          = 0x00,
+    .pannel_rx_gain          = 0x92,
+    .pannel_dump_shift       = 0x03,
+    .drv_frame_controle      = 0x1C,
+    .channel_level_up        = 0x00,
+    .module_switch3          = 0x00,
+    .gesture_dis             = 0x00,
     .gesture_long_press_time = 0x00,
 
-    .x_y_slope_adjust = 0x00,
-    .gesture_control = 0x03,
-    .gesture_switch1 = 0x64,
-    .gesture_switch2 = 0x32,
+    .x_y_slope_adjust     = 0x00,
+    .gesture_control      = 0x03,
+    .gesture_switch1      = 0x64,
+    .gesture_switch2      = 0x32,
     .gesture_refresh_rate = 0x00,
-    .gesture_touch_level = 0x00,
-    .newgreenwakeuplevel = 0x00,
-    .freq_hopping_start = 0x00,
-    .freq_hopping_end = 0x00,
-    .noise_detect_times = 0x00,
-    .hopping_flag = 0x00,
+    .gesture_touch_level  = 0x00,
+    .newgreenwakeuplevel  = 0x00,
+    .freq_hopping_start   = 0x00,
+    .freq_hopping_end     = 0x00,
+    .noise_detect_times   = 0x00,
+    .hopping_flag         = 0x00,
 
-    .hopping_threshold = 0x00,
-    .noise_threshold = 0x00,
-    .noise_min_threshold = 0x00,
-    .nc_8081 = 0x00,
-    .hopping_sensor_group = 0x00,
+    .hopping_threshold      = 0x00,
+    .noise_threshold        = 0x00,
+    .noise_min_threshold    = 0x00,
+    .nc_8081                = 0x00,
+    .hopping_sensor_group   = 0x00,
     .hopping_seg1_normalize = 0x00,
-    .hopping_seg1_factor = 0x00,
-    .main_clock_adjust = 0x00,
+    .hopping_seg1_factor    = 0x00,
+    .main_clock_adjust      = 0x00,
     .hopping_seg2_normalize = 0x00,
-    .hopping_seg2_factor = 0x00,
-    .nc_8088 = 0x00,
+    .hopping_seg2_factor    = 0x00,
+    .nc_8088                = 0x00,
 
     .hopping_seg3_normalize = 0x00,
-    .hopping_seg3_factor = 0x00,
-    .nc_808b = 0x00,
+    .hopping_seg3_factor    = 0x00,
+    .nc_808b                = 0x00,
     .hopping_seg4_normalize = 0x00,
-    .hopping_seg4_factor = 0x00,
-    .nc_808e = 0x00,
+    .hopping_seg4_factor    = 0x00,
+    .nc_808e                = 0x00,
     .hopping_seg5_normalize = 0x00,
-    .hopping_seg5_factor = 0x00,
-    .nc_8091 = 0x00,
+    .hopping_seg5_factor    = 0x00,
+    .nc_8091                = 0x00,
     .hopping_seg6_normalize = 0x00,
-    .key1 = 0x00,
+    .key1                   = 0x00,
 
-    .key2 = 0x00,
-    .key3 = 0x00,
-    .key4 = 0x00,
-    .key_area = 0x00,
-    .key_touch_level = 0x00,
-    .key_leave_level = 0x00,
-    .key_sens1 = 0x00,
-    .key_sens2 = 0x00,
-    .key_restrain = 0x00,
-    .key_restrain_time = 0x00,
+    .key2                = 0x00,
+    .key3                = 0x00,
+    .key4                = 0x00,
+    .key_area            = 0x00,
+    .key_touch_level     = 0x00,
+    .key_leave_level     = 0x00,
+    .key_sens1           = 0x00,
+    .key_sens2           = 0x00,
+    .key_restrain        = 0x00,
+    .key_restrain_time   = 0x00,
     .large_touch_gesture = 0x00,
 
-    .nc_809f = 0x00,
-    .nc_80a0 = 0x00,
+    .nc_809f           = 0x00,
+    .nc_80a0           = 0x00,
     .hotknot_noise_map = 0x00,
-    .link_threshold = 0x00,
-    .pxy_threshold = 0x00,
-    .ghot_dump_shift = 0x00,
-    .ghot_rx_gain = 0x00,
-    .freq_gain0 = 0x00,
-    .freq_gain1 = 0x00,
-    .freq_gain2 = 0x00,
-    .freq_gain3 = 0x00,
+    .link_threshold    = 0x00,
+    .pxy_threshold     = 0x00,
+    .ghot_dump_shift   = 0x00,
+    .ghot_rx_gain      = 0x00,
+    .freq_gain0        = 0x00,
+    .freq_gain1        = 0x00,
+    .freq_gain2        = 0x00,
+    .freq_gain3        = 0x00,
 
-    .gap0 = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    .gap0        = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
     .combine_dis = 0x00,
-    .split_set = 0x00,
+    .split_set   = 0x00,
 
-    .gap1 = {0x00, 0x00},
-    .sensor_ch = {0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0x0E, 0x10, 0x12, 0x14,
-                  0x16, 0x18, 0x1A, 0x1C},
-    .gap2 = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-             0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
-    .driver_ch = {0x00, 0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0xF,  0x10,
-                  0x12, 0x13, 0x14, 0x16, 0x18, 0x1C, 0x1D, 0x1E, 0x1F,
-                  0x20, 0x21, 0x22, 0x24, 0x26, 0x28, 0x29, 0x2A},
+    .gap1      = {0x00, 0x00},
+    .sensor_ch = {0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0x0E, 0x10, 0x12, 0x14, 0x16, 0x18, 0x1A,
+                  0x1C},
+    .gap2 = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+             0xFF, 0xFF},
+    .driver_ch = {0x00, 0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0xF,  0x10, 0x12, 0x13, 0x14, 0x16,
+                  0x18, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x24, 0x26, 0x28, 0x29, 0x2A},
     //    .sensor_ch = {0x1A, 0x18, 0x16, 0x14, 0x12, 0x10, 0x0E, 0x0C, 0x0A,
     //    0x08, 0x06, 0x04, 0x02, 0x00}, .gap2 = {0x00, 0x00, 0x00, 0x00, 0x00,
     //    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
     //    .driver_ch = {0x29, 0x28, 0x24, 0x22, 0x20, 0x1F, 0x1E, 0x1D, 0x0E,
     //    0x0C, 0x0A, 0x08, 0x06, 0x05, 0x04, 0x02, 0x00, 0xFF, 0x00, 0x00,
     //    0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-    .gap3 = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-             0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
+    .gap3   = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+               0xFF, 0xFF},
     .chksum = 0x00,
-    .fresh = 0x01,
+    .fresh  = 0x01,
 };
 
-static TX_THREAD touchpad_thread;
+static TX_THREAD            touchpad_thread;
 static TX_EVENT_FLAGS_GROUP touchpad_efg;
 
-void gt911_reset() {
+void gt911_reset()
+{
   TP_INT_GPIO_Port->PUPDR |= 0x01 << 14;
   HAL_GPIO_WritePin(TP_RESET_GPIO_Port, TP_RESET_Pin, GPIO_PIN_RESET);
   tx_thread_sleep(50);
@@ -288,70 +288,73 @@ void gt911_reset() {
 
 void thread_touchpad_entry(ULONG thread_input);
 
-uint8_t thread_touchpad_create(void) {
+uint8_t thread_touchpad_create(void)
+{
   UINT ret = TX_SUCCESS;
   //  void *stack = malloc(DEFAULT_APP_STACK_SIZE);
   ret = tx_thread_create(&touchpad_thread, "LTDC", thread_touchpad_entry, 0, stack,
-                         DEFAULT_APP_STACK_SIZE, 10, 10, TX_NO_TIME_SLICE,
-                         TX_AUTO_START);
+                         DEFAULT_APP_STACK_SIZE, 10, 10, TX_NO_TIME_SLICE, TX_AUTO_START);
 
   return ret;
 }
 
-uint8_t gt911_d = 0;
+uint8_t  gt911_d = 0;
 uint32_t gt911_x = 0;
 uint32_t gt911_y = 0;
 
-uint8_t thread_touchpad_sample(int32_t *x, int32_t *y) {
- *x = gt911_x;
- *y = gt911_y;
- return gt911_d;
+uint8_t thread_touchpad_sample(int32_t *x, int32_t *y)
+{
+  *x = gt911_x;
+  *y = gt911_y;
+  return gt911_d;
 }
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
   tx_event_flags_set(&touchpad_efg, 0x01, TX_OR);
 }
 
-HAL_StatusTypeDef gt911_read_pid() {
+HAL_StatusTypeDef gt911_read_pid()
+{
   gt911_config_t valid_config;
 
   HAL_StatusTypeDef status;
-  uint8_t info_bytes[0x0B];
-  status =
-      Bambloo_I2C_Mem_Read(gt911_address, 0x8140, info_bytes, sizeof(info_bytes), 2);
+  uint8_t           info_bytes[0x0B];
+  status = Bambloo_I2C_Mem_Read(gt911_address, 0x8140, info_bytes, sizeof(info_bytes), 2);
   // status = BamblooI2C_Mem_Read(gt911_address, 0x8047, (uint8_t *)&valid_config,
   //                          sizeof(valid_config));
   tx_thread_sleep(50);
   gt911_config.config_version = valid_config.config_version + 1;
-  uint8_t checksum = 0;
+  uint8_t checksum            = 0;
   for (int i = 0; i < sizeof(gt911_config) - 2; i++) {
     checksum += ((uint8_t *)&gt911_config)[i];
   }
-  checksum = (~checksum);
+
+  checksum            = (~checksum);
   gt911_config.chksum = checksum + 1;
-  gt911_config.fresh = 1;
-  status = Bambloo_I2C_Mem_Write(gt911_address, 0x8047, (uint8_t *)&gt911_config,
-                            sizeof(gt911_config), 2);
+  gt911_config.fresh  = 1;
+  status              = Bambloo_I2C_Mem_Write(gt911_address, 0x8047, (uint8_t *)&gt911_config,
+                                              sizeof(gt911_config), 2);
   tx_thread_sleep(20);
 
   status = Bambloo_I2C_Mem_Read(gt911_address, 0x8047, (uint8_t *)&valid_config,
-                           sizeof(valid_config), 2);
+                                sizeof(valid_config), 2);
   tx_thread_sleep(20);
   return status;
 }
 
-void thread_touchpad_entry(ULONG thread_input) {
+void thread_touchpad_entry(ULONG thread_input)
+{
   uint32_t flags;
   uint32_t count = 0;
-  uint8_t status_register, temp_register = 0, coors[4];
+  uint8_t  status_register, temp_register = 0, coors[4];
 
 reinit:
   gt911_reset();
   gt911_read_pid();
 
   while (1) {
-    tx_event_flags_get(&touchpad_efg, 0x01, TX_AND_CLEAR, &flags,
-                       TX_WAIT_FOREVER);
+    tx_event_flags_get(&touchpad_efg, 0x01, TX_AND_CLEAR, &flags, TX_WAIT_FOREVER);
 
     HAL_StatusTypeDef status =
         Bambloo_I2C_Mem_Read(gt911_address, 0x814E, &status_register, 0x01, 2);
@@ -389,7 +392,6 @@ reinit:
       continue;
     }
 
-    
     gt911_d = 1;
 
     gt911_y = coors[0] | coors[1] << 8;

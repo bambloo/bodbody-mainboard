7e5817d4e9775f1ece053d116f798efcc520d084
Eric Liu <liugchome2004@163.com>
2026-06-25 14:29:34 +0800

    blitCopy

--- Drivers/User/Src/touchgfx/HAL.cpp
+++ Drivers/User/Src/touchgfx/HAL.cpp
@@ -92,6 +92,15 @@ void HAL::blitCopy(const uint16_t *pSrc, const uint8_t *pClut, uint16_t x,
   dma.addToQueue(dop);
 }
 
+void HAL::blitCopy(const uint16_t *pSrc, uint16_t x, uint16_t y, uint16_t width,
+                   uint16_t height, uint16_t srcWidth, uint8_t alpha,
+                   bool hasTransparentPixels, bool replaceBgAlpha) {
+  auto format = lcd().framebufferFormat();
+  return blitCopy(pSrc, nullptr, x, y, width, height, srcWidth, alpha,
+                  hasTransparentPixels, FRAME_BUFFER_WIDTH, format, format,
+                  replaceBgAlpha);
+}
+
 void HAL::taskEntry() {
   enableLCDControllerInterrupt();
   enableInterrupts();
